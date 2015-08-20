b5d861ec09fa18ca2efa887f754df3a3c31083b2
Maximilian Stammnitz <mrs72@cam.ac.uk>
2015-08-20 18:04:01 +0100

    readme-file contained unrecognisable header lines.

--- 1_Indelwald_functions.R
+++ 1_Indelwald_functions.R
@@ -881,8 +881,11 @@ devil.transcripts <- read.table("Devil_Transcript_7.1.txt", header=T, sep=";")
 devil.genes.trans <- read.table("Devil_Genes_7.1_ENSEMBL_translated.txt", header=T, sep=";")
 devil.transcripts.trans <- read.table("Devil_Transcript_7.1_ENSEMBL_translated.txt", header=T, sep=";")
 <<<<<<< HEAD
+<<<<<<< HEAD
 #all.contigs <- read.table("Contig_Translate.txt")
 #cosmic.genes <- read.csv("COSMIC_Drivers.csv")
+=======
+>>>>>>> master
 =======
 >>>>>>> master
 setwd(main.path)
